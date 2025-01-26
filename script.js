936510c633fc1481e4ad966250c8229d7b2dbfc4
Kiaze Wgna <kaizewang9494@gmail.com>
2025-01-26 15:23:27 -0500

    changed aspect ratio, added assets, centralized constants in one place

--- script.js
+++ script.js
@@ -2,8 +2,32 @@ window.addEventListener("load",function(){
     //setup of canvas
     const canvas = document.getElementById("canvas1");
     const ctx = canvas.getContext("2d");
-    canvas.width = 1500;
-    canvas.height = 500;
+    //initial constants setup
+    const height= 500;
+    const realheight=15;
+    const heighttowidth=5;
+    const floorfromtop=realheight-2;
+    const realwidth=realheight*heighttowidth;
+    const meter_scale=height/realheight;
+    const width = realwidth*meter_scale;
+    canvas.width = width;
+    canvas.height = height;
+    //calculated values setup
+    const initial_time_scale=1;
+    const realdinosaurheight=5.3;
+    const realdinosaurwidth=2;
+    const realbirdheight=2;
+    const realbirdwidth=5.3;
+    const dinoheight=realdinosaurheight*meter_scale;
+    const dinowidth=realdinosaurwidth*meter_scale;
+    const birdheight=realbirdheight*meter_scale;
+    const birdwidth=realbirdwidth*meter_scale;
+    const floor=(floorfromtop-realdinosaurheight)*meter_scale;
+    const gravity=9.8*meter_scale;
+    const realjumpingspeed=3.27*3;
+    const jumpspeed=realjumpingspeed*meter_scale;
+    
+    //Classes
     class InputHandler {
         constructor(game){
             this.game = game;
@@ -36,7 +60,7 @@ window.addEventListener("load",function(){
             this.alive=true;
         }
         update(){
-            this.x=this.x+this.speed*this.game.ingametime;
+            this.x=this.x+this.speed*this.game.time;
             if (this.x>this.game.width*0.8) this.alive=false;
         }
         draw(context){
@@ -52,31 +76,35 @@ window.addEventListener("load",function(){
     class Player{
         constructor(game){
             this.game=game;
+<<<<<<< HEAD
             this.width =120;
             this.height=200;
+=======
+            this.width =dinowidth;
+            this.height=dinoheight;
+>>>>>>> 9e3d2cd (changed aspect ratio, added assets, centralized constants in one place)
             this.x=20;
             this.y=100;
             this.speedY=0;
-            this.initialspeedY=5;  
             this.projectiles=[];
+            this.player1=document.getElementById("player1");
+            this.playerh2w=this.player1.height/this.player1.width;
         } 
         update(){
             if ((this.game.keys.includes(" "))&&(this.speedY==0)) {
-                this.speedY= 0-(this.initialspeedY*this.game.meter_scale);
+                this.speedY=0-jumpspeed;
             }
             // gravity
-            if ((this.speedY!=0)||(this.y<this.game.floor_y)){
-                const gravity=9.8*this.game.meter_scale
-                const next_y= this.y + (this.speedY*this.game.ingametime) + (0.5*gravity*(this.game.ingametime**2))
-                if (next_y>=this.game.floor_y){
-                    this.y=this.game.floor_y;
+            if ((this.speedY!=0)||(this.y<this.game.floor)){
+                const next_y= this.y + (this.speedY*this.game.time) + (0.5*gravity*(this.game.time**2))
+                if (next_y>=this.game.floor){
+                    this.y=this.game.floor;
                     this.speedY=0;
                 }
                 else{
                     this.y=next_y;
-                    this.speedY=this.speedY+gravity*this.game.ingametime;
+                    this.speedY=this.speedY+gravity*this.game.time;
                 }
-                console.log([next_y,this.y,this.speedY,gravity,this.game.ingametime])
             } 
 
             // projectile
@@ -90,6 +118,7 @@ window.addEventListener("load",function(){
         draw(context){
             context.fillstyle="black";
             context.fillRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.player1,this.x, this.y,this.height/this.playerh2w,this.height)
             this.projectiles.forEach(projectile => {
                 projectile.draw(context);
             })
@@ -111,33 +140,41 @@ window.addEventListener("load",function(){
 
     }
     class Game{
-        constructor(width,height){
+        constructor(){
             this.width = width;
             this.height = height;
+            this.floor=floor;
+            this.meter_scale=meter_scale;
+            this.time_scale=initial_time_scale;
             this.player=new Player(this);
             this.input=new InputHandler(this);
             this.keys=[];
-            this.floor_y=300
-            this.realheight=3;
-            this.meter_scale=this.height/this.realheight;
-            this.time_scale=0.8;
             this.last_time=performance.now()
             this.current_time=performance.now()
             this.delta_time=0
-            this.ingametime=this.delta_time*this.time_scale
+            this.time=this.delta_time*this.time_scale
         }
         update(){
             this.current_time=performance.now()
-            this.delta_time=(this.current_time-this.last_time)/1000
-            this.ingametime=this.delta_time*this.time_scale
+            this.delta_time=(this.current_time-this.last_time)/1000 
+            this.time=this.delta_time*this.time_scale
             this.last_time=this.current_time
             this.player.update();
         }
         draw(context){
             this.player.draw(context);
         }
+        detect_collision(obj1,obj2){
+            return(
+                obj1.x<obj2.x+obj2.width &&
+                obj1.x+obj1.width>obj2.x &&
+                obj1.y<obj2.y+obj2.height &&
+                obj1.y+obj1.height>obj2.y
+            )
+        }
     }
-    const game= new Game(canvas.width,canvas.height);
+    //actual code
+    const game= new Game();
     function animation(){
         ctx.clearRect(0,0,canvas.width,canvas.height);
         game.update();
